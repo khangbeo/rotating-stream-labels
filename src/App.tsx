8b918cd8a6c22c10ef227d87574d5ed85789d78f
Anthony Duong <khangbeo2012@gmail.com>
2024-08-05 16:41:12 -0700

    added a fallback in case fetching fails, use data from localstorage instead

--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,70 @@ import useCarousel from "./hooks/useCarousel";
 import { User } from "./types/User";
 
 function App() {
-  const [users, setUsers] = useLocalStorage<User[]>("users", []);
-  const [lastFetch, setLastFetch] = useLocalStorage("lastFetch", 0);
-  const [error, setError] = useState<string | null>("");
-  const index = useCarousel(users, 5000);
+    const [users, setUsers] = useLocalStorage<User[]>("users", []);
+    const [lastFetch, setLastFetch] = useLocalStorage("lastFetch", 0);
+    const [error, setError] = useState<string | null>("");
+    const index = useCarousel(users, 5000);
 
-  const ONE_DAY = 24 * 60 * 60 * 1000; // milliseconds in one day
+    const ONE_DAY = 24 * 60 * 60 * 1000; // milliseconds in one day
 
-  useEffect(() => {
-    const now = Date.now();
+    useEffect(() => {
+        const now = Date.now();
 
-    if (!lastFetch || now - lastFetch > ONE_DAY) {
-      fetchData()
-        .then((userData) => {
-          setUsers(userData);
-          setLastFetch(now);
-        })
-        .catch((error) => {
-          setError("Failed to load user data.");
-          console.error("Error fetching user data: ", error);
-        });
-    } else {
-      console.log("Data fetched less than 24 hours ago. Skipping fetch.");
+        if (!lastFetch || now - lastFetch > ONE_DAY) {
+            fetchData()
+                .then((userData) => {
+                    setUsers(userData);
+                    setLastFetch(now);
+                })
+                .catch((error) => {
+                    setError("Failed to load user data.");
+                    console.error("Error fetching user data: ", error);
+                });
+        } else {
+            console.log("Data fetched less than 24 hours ago. Skipping fetch.");
+        }
+    }, [lastFetch, setLastFetch, setUsers]);
+
+    if (error) {
+        // return (
+        //   <div className="flex justify-center items-center h-screen">
+        //     <p>{error}</p>
+        //   </div>
+        // );
+        // if error then return what's already cached
+        return (
+            <div className="flex justify-center items-center h-screen font-PO">
+                <div className="relative w-80 h-80">
+                    {users.map((user: any, i: number) => (
+                        <UserDisplay
+                            key={i}
+                            user={user}
+                            isVisible={i === index}
+                        />
+                    ))}
+                </div>
+            </div>
+        );
     }
-  }, [lastFetch, setLastFetch, setUsers]);
 
-  if (error) {
+    if (users.length === 0) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <p>Loading...</p>
+            </div>
+        );
+    }
+
     return (
-      <div className="flex justify-center items-center h-screen">
-        <p>{error}</p>
-      </div>
+        <div className="flex justify-center items-center h-screen font-PO">
+            <div className="relative w-80 h-80">
+                {users.map((user: any, i: number) => (
+                    <UserDisplay key={i} user={user} isVisible={i === index} />
+                ))}
+            </div>
+        </div>
     );
-  }
-
-  if (users.length === 0) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
-  return (
-    <div className="flex justify-center items-center h-screen font-PO">
-      <div className="relative w-80 h-80">
-        {users.map((user: any, i: number) => (
-          <UserDisplay key={i} user={user} isVisible={i === index} />
-        ))}
-      </div>
-    </div>
-  );
 }
 
 export default App;
